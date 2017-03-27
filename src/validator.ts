fed10e551069fc87ec81716043044550dcdec9bd
nivinjoseph <nivinjoseph@outlook.com>
2017-03-26 21:57:14 -0400

    resolved property name inconsistency

--- src/validator.ts
+++ src/validator.ts
@@ -22,7 +22,7 @@ export default class Validator<T> implements ValidationInitializer<T>, Validatio
     {
         given(propertyName, "propertyName")
             .ensureHasValue()
-            .ensure(t => this._propertyValidators.every(u => u.PropertyName !== t),
+            .ensure(t => this._propertyValidators.every(u => u.propertyName !== t),
             "validation already defined for property '{0}'".format(propertyName));
 
         let propertyValidator = new InternalPropertyValidator<T, TProperty>(propertyName);
@@ -37,11 +37,11 @@ export default class Validator<T> implements ValidationInitializer<T>, Validatio
 
         this._propertyValidators.forEach(t =>
         {
-            t.Validate(value);
-            if (t.HasError)
+            t.validate(value);
+            if (t.hasError)
             {
                 this._hasErrors = true;
-                this._errors[t.PropertyName] = t.Error;
+                this._errors[t.propertyName] = t.error;
             }
         });
     }
@@ -49,16 +49,16 @@ export default class Validator<T> implements ValidationInitializer<T>, Validatio
 
 class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TProperty>
 {
-    private _propertyName: string;
+    private readonly _propertyName: string;
     private _hasError: boolean;
     private _error: any;
     private readonly _validationRules = new Array<InternalPropertyValidationRule<T, TProperty>>();
     private _lastValidationRule: InternalPropertyValidationRule<T, TProperty>;
 
 
-    public get PropertyName(): string { return this._propertyName; }
-    public get HasError(): boolean { return this._hasError; }
-    public get Error(): string { return this._error; }
+    public get propertyName(): string { return this._propertyName; }
+    public get hasError(): boolean { return this._hasError; }
+    public get error(): string { return this._error; }
 
 
     public constructor(propertyName: string)
@@ -67,7 +67,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
     }
 
 
-    public Validate(value: T): void
+    public validate(value: T): void
     {
         this._hasError = false;
         this._error = null;
@@ -83,7 +83,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
 
             try
             {
-                validationResult = validationRule.Validate(val, propertyVal);
+                validationResult = validationRule.validate(val, propertyVal);
             } catch (e)
             {
                 if (e === "OPTIONAL")
@@ -93,7 +93,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
             if (!validationResult)
             {
                 this._hasError = true;
-                this._error = validationRule.Error;
+                this._error = validationRule.error;
                 break;
             }
         }
@@ -102,7 +102,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
     public isRequired(): PropertyValidator<T, TProperty>
     {
         this._lastValidationRule = new InternalPropertyValidationRule<T, TProperty>();
-        this._lastValidationRule.Ensure((propertyValue: TProperty) =>
+        this._lastValidationRule.ensure((propertyValue: TProperty) =>
         {
             if (propertyValue != null)
             {
@@ -115,7 +115,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
             return false;
         });
 
-        this._lastValidationRule.WithMessage("Required");
+        this._lastValidationRule.withMessage("Required");
         this._validationRules.push(this._lastValidationRule);
         return this;
     }
@@ -123,7 +123,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
     public isOptional(): PropertyValidator<T, TProperty>
     {
         this._lastValidationRule = new InternalPropertyValidationRule<T, TProperty>();
-        this._lastValidationRule.Ensure((propertyValue: TProperty) =>
+        this._lastValidationRule.ensure((propertyValue: TProperty) =>
         {
             if (propertyValue == null)
                 throw "OPTIONAL";
@@ -141,7 +141,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
     public ensure(propertyValidationPredicate: (propertyValue: TProperty) => boolean): PropertyValidator<T, TProperty>
     {
         this._lastValidationRule = new InternalPropertyValidationRule<T, TProperty>();
-        this._lastValidationRule.Ensure(propertyValidationPredicate);
+        this._lastValidationRule.ensure(propertyValidationPredicate);
         this._validationRules.push(this._lastValidationRule);
         return this;
     }
@@ -149,7 +149,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
     public ensureT(valueValidationPredicate: (value: T) => boolean): PropertyValidator<T, TProperty>
     {
         this._lastValidationRule = new InternalPropertyValidationRule<T, TProperty>();
-        this._lastValidationRule.EnsureT(valueValidationPredicate);
+        this._lastValidationRule.ensureT(valueValidationPredicate);
         this._validationRules.push(this._lastValidationRule);
         return this;
     }
@@ -157,7 +157,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
     public useValidationRule(validationRule: ValidationRule<TProperty>): PropertyValidator<T, TProperty>
     {
         this._lastValidationRule = new InternalPropertyValidationRule<T, TProperty>();
-        this._lastValidationRule.UseValidationRule(validationRule);
+        this._lastValidationRule.useValidationRule(validationRule);
         this._validationRules.push(this._lastValidationRule);
         return this;
     }
@@ -165,7 +165,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
     public useValidator(validator: Validator<TProperty>): PropertyValidator<T, TProperty>
     {
         this._lastValidationRule = new InternalPropertyValidationRule<T, TProperty>();
-        this._lastValidationRule.UseValidator(validator);
+        this._lastValidationRule.useValidator(validator);
         this._validationRules.push(this._lastValidationRule);
         return this;
     }
@@ -177,7 +177,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
         if (this._lastValidationRule == null)
             throw new ApplicationException("No target Validation Rule specified for the condition.");
 
-        this._lastValidationRule.If(conditionPredicate);
+        this._lastValidationRule.if(conditionPredicate);
 
         return this;
     }
@@ -189,7 +189,7 @@ class InternalPropertyValidator<T, TProperty> implements PropertyValidator<T, TP
         if (this._lastValidationRule == null)
             throw new ApplicationException("No target Validation Rule specified for the condition.");
 
-        this._lastValidationRule.WithMessage(errorMessage);
+        this._lastValidationRule.withMessage(errorMessage);
 
         return this;
     }
@@ -206,7 +206,7 @@ class InternalPropertyValidationRule<T, TProperty>
     private _overrideError = false;
 
 
-    public get Error(): Object
+    public get error(): Object
     {
         if (this._validationRule != null && !this._overrideError)
             return this._validationRule.error;
@@ -216,7 +216,7 @@ class InternalPropertyValidationRule<T, TProperty>
     }
 
 
-    public Ensure(tpropertyValidationPredicate: (propertyValue: TProperty) => boolean): void
+    public ensure(tpropertyValidationPredicate: (propertyValue: TProperty) => boolean): void
     {
         given(tpropertyValidationPredicate, "tpropertyValidationPredicate").ensureHasValue();
 
@@ -224,7 +224,7 @@ class InternalPropertyValidationRule<T, TProperty>
         this._error = "Invalid value";
     }
 
-    public EnsureT(tValidationPredicate: (value: T) => boolean): void
+    public ensureT(tValidationPredicate: (value: T) => boolean): void
     {    
         given(tValidationPredicate, "tValidationPredicate").ensureHasValue();
         
@@ -232,28 +232,28 @@ class InternalPropertyValidationRule<T, TProperty>
         this._error = "Invalid value";
     }
 
-    public UseValidationRule(validationRule: ValidationRule<TProperty>): void
+    public useValidationRule(validationRule: ValidationRule<TProperty>): void
     {
         given(validationRule, "validationRule").ensureHasValue();
 
         this._validationRule = validationRule;
     }
 
-    public UseValidator(validator: Validator<TProperty>): void
+    public useValidator(validator: Validator<TProperty>): void
     {
         given(validator, "validator").ensureHasValue();
         
         this._validator = validator;
     }
 
-    public If(conditionPredicate: (value: T) => boolean): void
+    public if(conditionPredicate: (value: T) => boolean): void
     {
         given(conditionPredicate, "conditionPredicate").ensureHasValue();
         
         this._conditionPredicate = conditionPredicate;
     }
 
-    public WithMessage(errorMessage: string): void
+    public withMessage(errorMessage: string): void
     {
         given(errorMessage, "errorMessage").ensureHasValue().ensure(t => !t.isEmptyOrWhiteSpace());
 
@@ -261,7 +261,7 @@ class InternalPropertyValidationRule<T, TProperty>
         this._overrideError = true;
     }
 
-    public Validate(value: T, propertyValue: TProperty): boolean
+    public validate(value: T, propertyValue: TProperty): boolean
     {
         if (this._conditionPredicate != null && !this._conditionPredicate(value))
             return true;
