24b0833d15ab72778363bca6b689ab7ffb4ed992
nivinjoseph <nivinjoseph@outlook.com>
2017-03-26 21:57:30 -0400

    preparing to publish new version

--- dist/validator.js
+++ dist/validator.js
@@ -33,7 +33,7 @@ var Validator = (function () {
         var _this = this;
         n_defensive_1.default(propertyName, "propertyName")
             .ensureHasValue()
-            .ensure(function (t) { return _this._propertyValidators.every(function (u) { return u.PropertyName !== t; }); }, "validation already defined for property '{0}'".format(propertyName));
+            .ensure(function (t) { return _this._propertyValidators.every(function (u) { return u.propertyName !== t; }); }, "validation already defined for property '{0}'".format(propertyName));
         var propertyValidator = new InternalPropertyValidator(propertyName);
         this._propertyValidators.push(propertyValidator);
         return propertyValidator;
@@ -43,10 +43,10 @@ var Validator = (function () {
         this._hasErrors = false;
         this._errors = new Object();
         this._propertyValidators.forEach(function (t) {
-            t.Validate(value);
-            if (t.HasError) {
+            t.validate(value);
+            if (t.hasError) {
                 _this._hasErrors = true;
-                _this._errors[t.PropertyName] = t.Error;
+                _this._errors[t.propertyName] = t.error;
             }
         });
     };
@@ -58,22 +58,22 @@ var InternalPropertyValidator = (function () {
         this._validationRules = new Array();
         this._propertyName = propertyName;
     }
-    Object.defineProperty(InternalPropertyValidator.prototype, "PropertyName", {
+    Object.defineProperty(InternalPropertyValidator.prototype, "propertyName", {
         get: function () { return this._propertyName; },
         enumerable: true,
         configurable: true
     });
-    Object.defineProperty(InternalPropertyValidator.prototype, "HasError", {
+    Object.defineProperty(InternalPropertyValidator.prototype, "hasError", {
         get: function () { return this._hasError; },
         enumerable: true,
         configurable: true
     });
-    Object.defineProperty(InternalPropertyValidator.prototype, "Error", {
+    Object.defineProperty(InternalPropertyValidator.prototype, "error", {
         get: function () { return this._error; },
         enumerable: true,
         configurable: true
     });
-    InternalPropertyValidator.prototype.Validate = function (value) {
+    InternalPropertyValidator.prototype.validate = function (value) {
         this._hasError = false;
         this._error = null;
         var val = value;
@@ -82,7 +82,7 @@ var InternalPropertyValidator = (function () {
             var validationRule = this._validationRules[i];
             var validationResult = true;
             try {
-                validationResult = validationRule.Validate(val, propertyVal);
+                validationResult = validationRule.validate(val, propertyVal);
             }
             catch (e) {
                 if (e === "OPTIONAL")
@@ -90,14 +90,14 @@ var InternalPropertyValidator = (function () {
             }
             if (!validationResult) {
                 this._hasError = true;
-                this._error = validationRule.Error;
+                this._error = validationRule.error;
                 break;
             }
         }
     };
     InternalPropertyValidator.prototype.isRequired = function () {
         this._lastValidationRule = new InternalPropertyValidationRule();
-        this._lastValidationRule.Ensure(function (propertyValue) {
+        this._lastValidationRule.ensure(function (propertyValue) {
             if (propertyValue != null) {
                 if ((typeof propertyValue) === "string") {
                     return !propertyValue.isEmptyOrWhiteSpace();
@@ -106,13 +106,13 @@ var InternalPropertyValidator = (function () {
             }
             return false;
         });
-        this._lastValidationRule.WithMessage("Required");
+        this._lastValidationRule.withMessage("Required");
         this._validationRules.push(this._lastValidationRule);
         return this;
     };
     InternalPropertyValidator.prototype.isOptional = function () {
         this._lastValidationRule = new InternalPropertyValidationRule();
-        this._lastValidationRule.Ensure(function (propertyValue) {
+        this._lastValidationRule.ensure(function (propertyValue) {
             if (propertyValue == null)
                 throw "OPTIONAL";
             if ((typeof propertyValue) === "string" && propertyValue.isEmptyOrWhiteSpace())
@@ -124,25 +124,25 @@ var InternalPropertyValidator = (function () {
     };
     InternalPropertyValidator.prototype.ensure = function (propertyValidationPredicate) {
         this._lastValidationRule = new InternalPropertyValidationRule();
-        this._lastValidationRule.Ensure(propertyValidationPredicate);
+        this._lastValidationRule.ensure(propertyValidationPredicate);
         this._validationRules.push(this._lastValidationRule);
         return this;
     };
     InternalPropertyValidator.prototype.ensureT = function (valueValidationPredicate) {
         this._lastValidationRule = new InternalPropertyValidationRule();
-        this._lastValidationRule.EnsureT(valueValidationPredicate);
+        this._lastValidationRule.ensureT(valueValidationPredicate);
         this._validationRules.push(this._lastValidationRule);
         return this;
     };
     InternalPropertyValidator.prototype.useValidationRule = function (validationRule) {
         this._lastValidationRule = new InternalPropertyValidationRule();
-        this._lastValidationRule.UseValidationRule(validationRule);
+        this._lastValidationRule.useValidationRule(validationRule);
         this._validationRules.push(this._lastValidationRule);
         return this;
     };
     InternalPropertyValidator.prototype.useValidator = function (validator) {
         this._lastValidationRule = new InternalPropertyValidationRule();
-        this._lastValidationRule.UseValidator(validator);
+        this._lastValidationRule.useValidator(validator);
         this._validationRules.push(this._lastValidationRule);
         return this;
     };
@@ -150,14 +150,14 @@ var InternalPropertyValidator = (function () {
         n_defensive_1.default(conditionPredicate, "conditionPredicate").ensureHasValue();
         if (this._lastValidationRule == null)
             throw new n_exception_1.ApplicationException("No target Validation Rule specified for the condition.");
-        this._lastValidationRule.If(conditionPredicate);
+        this._lastValidationRule.if(conditionPredicate);
         return this;
     };
     InternalPropertyValidator.prototype.withMessage = function (errorMessage) {
         n_defensive_1.default(errorMessage, "errorMessage").ensureHasValue();
         if (this._lastValidationRule == null)
             throw new n_exception_1.ApplicationException("No target Validation Rule specified for the condition.");
-        this._lastValidationRule.WithMessage(errorMessage);
+        this._lastValidationRule.withMessage(errorMessage);
         return this;
     };
     return InternalPropertyValidator;
@@ -166,7 +166,7 @@ var InternalPropertyValidationRule = (function () {
     function InternalPropertyValidationRule() {
         this._overrideError = false;
     }
-    Object.defineProperty(InternalPropertyValidationRule.prototype, "Error", {
+    Object.defineProperty(InternalPropertyValidationRule.prototype, "error", {
         get: function () {
             if (this._validationRule != null && !this._overrideError)
                 return this._validationRule.error;
@@ -178,34 +178,34 @@ var InternalPropertyValidationRule = (function () {
         enumerable: true,
         configurable: true
     });
-    InternalPropertyValidationRule.prototype.Ensure = function (tpropertyValidationPredicate) {
+    InternalPropertyValidationRule.prototype.ensure = function (tpropertyValidationPredicate) {
         n_defensive_1.default(tpropertyValidationPredicate, "tpropertyValidationPredicate").ensureHasValue();
         this._tpropertyValidationPredicate = tpropertyValidationPredicate;
         this._error = "Invalid value";
     };
-    InternalPropertyValidationRule.prototype.EnsureT = function (tValidationPredicate) {
+    InternalPropertyValidationRule.prototype.ensureT = function (tValidationPredicate) {
         n_defensive_1.default(tValidationPredicate, "tValidationPredicate").ensureHasValue();
         this._tValidationPredicate = tValidationPredicate;
         this._error = "Invalid value";
     };
-    InternalPropertyValidationRule.prototype.UseValidationRule = function (validationRule) {
+    InternalPropertyValidationRule.prototype.useValidationRule = function (validationRule) {
         n_defensive_1.default(validationRule, "validationRule").ensureHasValue();
         this._validationRule = validationRule;
     };
-    InternalPropertyValidationRule.prototype.UseValidator = function (validator) {
+    InternalPropertyValidationRule.prototype.useValidator = function (validator) {
         n_defensive_1.default(validator, "validator").ensureHasValue();
         this._validator = validator;
     };
-    InternalPropertyValidationRule.prototype.If = function (conditionPredicate) {
+    InternalPropertyValidationRule.prototype.if = function (conditionPredicate) {
         n_defensive_1.default(conditionPredicate, "conditionPredicate").ensureHasValue();
         this._conditionPredicate = conditionPredicate;
     };
-    InternalPropertyValidationRule.prototype.WithMessage = function (errorMessage) {
+    InternalPropertyValidationRule.prototype.withMessage = function (errorMessage) {
         n_defensive_1.default(errorMessage, "errorMessage").ensureHasValue().ensure(function (t) { return !t.isEmptyOrWhiteSpace(); });
         this._error = errorMessage;
         this._overrideError = true;
     };
-    InternalPropertyValidationRule.prototype.Validate = function (value, propertyValue) {
+    InternalPropertyValidationRule.prototype.validate = function (value, propertyValue) {
         if (this._conditionPredicate != null && !this._conditionPredicate(value))
             return true;
         if (this._tpropertyValidationPredicate != null)
