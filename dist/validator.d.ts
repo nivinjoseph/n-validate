b084eff16945703fa64820f116c99e8f7ad4f4f6
nivinjoseph <nivinjoseph@outlook.com>
2018-04-12 20:57:44 -0400

    preparing to publish new version

--- dist/validator.d.ts
+++ dist/validator.d.ts
@@ -3,15 +3,20 @@ import { PropertyValidator } from "./property-validator";
 import { ValidationInitializer } from "./validation-initializer";
 import { ValidationExecutor } from "./validation-executor";
 export declare class Validator<T> implements ValidationInitializer<T>, ValidationExecutor<T> {
-    private _propertyValidators;
+    private readonly _propertyValidators;
+    private readonly _errors;
     private _hasErrors;
-    private _errors;
+    private _isEnabled;
     readonly isValid: boolean;
     readonly hasErrors: boolean;
     readonly errors: {
         [index: string]: any;
     };
     readonly hasRules: boolean;
+    readonly isEnabled: boolean;
+    constructor(isEnabled?: boolean);
     for<TProperty>(propertyName: string): PropertyValidator<T, TProperty>;
     validate(value: T): void;
+    enable(): void;
+    disable(): void;
 }
--- dist/validator.js
+++ dist/validator.js
@@ -5,15 +5,18 @@ require("@nivinjoseph/n-ext");
 const internal_property_validator_1 = require("./internal-property-validator");
 // public
 class Validator {
-    constructor() {
+    constructor(isEnabled = true) {
         this._propertyValidators = new Array();
-        this._hasErrors = false;
         this._errors = {};
+        this._hasErrors = false;
+        this._isEnabled = true;
+        this._isEnabled = isEnabled;
     }
     get isValid() { return !this._hasErrors; }
     get hasErrors() { return this._hasErrors; }
     get errors() { return this._errors; }
     get hasRules() { return this._propertyValidators.length > 0; }
+    get isEnabled() { return this._isEnabled; }
     for(propertyName) {
         n_defensive_1.given(propertyName, "propertyName")
             .ensureHasValue()
@@ -21,20 +24,28 @@ class Validator {
             .ensure(t => this._propertyValidators.every(u => u.propertyName !== t), "validation already defined for property '{0}'".format(propertyName));
         let propertyValidator = new internal_property_validator_1.InternalPropertyValidator(propertyName);
         this._propertyValidators.push(propertyValidator);
+        this._errors[propertyName] = null;
         return propertyValidator;
     }
     validate(value) {
+        n_defensive_1.given(value, "value").ensureHasValue();
         this._hasErrors = false;
-        this._errors = {};
-        n_defensive_1.given(value, "value")
-            .ensureHasValue();
-        this._propertyValidators.forEach(t => {
-            t.validate(value);
-            if (t.hasError) {
-                this._hasErrors = true;
-                this._errors[t.propertyName] = t.error;
-            }
-        });
+        if (this._isEnabled) {
+            this._propertyValidators.forEach(t => {
+                t.validate(value);
+                this._hasErrors = t.hasError;
+                this._errors[t.propertyName] = t.hasError ? t.error : null;
+            });
+        }
+        else {
+            this._propertyValidators.forEach(t => this._errors[t.propertyName] = null);
+        }
+    }
+    enable() {
+        this._isEnabled = true;
+    }
+    disable() {
+        this._isEnabled = false;
     }
 }
 exports.Validator = Validator;
