fc6baf3eb66af81661d6246c70ecfb08d3a7f5d5
Nivin Joseph <nivinjoseph@outlook.com>
2022-10-09 16:18:50 -0400

    preparing to publish new version

--- dist/string-validation/is-date.js
+++ dist/string-validation/is-date.js
@@ -3,7 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.isDate = void 0;
 const base_string_validation_rule_1 = require("./base-string-validation-rule");
 const n_defensive_1 = require("@nivinjoseph/n-defensive");
-const moment = require("moment");
+// import * as moment from "moment";
+const dayjs = require("dayjs");
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+dayjs.extend(customParseFormat);
 // public
 /**
  *
@@ -19,7 +22,7 @@ class StringIsDate extends base_string_validation_rule_1.BaseStringValidationRul
         super();
         this.addValidationRule({
             // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-            validate: t => t == null || moment(t, format).isValid(),
+            validate: t => t == null || dayjs(t, format, true).isValid(),
             error: "Invalid date"
         });
     }
